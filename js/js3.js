05b152a53921ce17d031989161043cdffc9b9097
Bordiuh <55415864+Bordiuh@users.noreply.github.com>
2020-11-17 21:07:30 +0200

    change class hero

--- js/js3.js
+++ js/js3.js
@@ -70,16 +70,27 @@ class hero{
     constructor(x,y) {
         this.x = x
         this.y = y
-            .up = (this.y++)
-            .down = (this.y--)
-            .left = (this.x--)
-            .right = (this.x++)
+
+    }
+    up() {
+        this.y++;
+    }
+    down(){
+        this.y--;
+    }
+    left(){
+        this.x--;
+    }
+    right(){
+        this.x++
     }
 }
 
 
 /* Вывод лабиринта */
 export function fieldOut(Lab) {
+
+    document.body.write();
 
     for (let i = 0; i < Lab.length; i++) {
         let rowConteiner = document.createElement("div");
